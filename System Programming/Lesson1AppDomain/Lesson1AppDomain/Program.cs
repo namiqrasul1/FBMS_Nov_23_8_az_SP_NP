132affd7b44a8ad664e44b7e528e209aeb8dd430
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-09-12 11:13:21 +0400

    Lesson 1. Multitasking, Multithreading, Process, AppDomain

---
+++ System Programming/Lesson1AppDomain/Lesson1AppDomain/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security;
+using System.Security.Permissions;
+
+namespace Lesson1AppDomain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //AppDomain domain = AppDomain.CreateDomain("BabatDomain");
+
+            //Type type = typeof(Virus);
+            ////Console.WriteLine(type.FullName);
+            ////Console.WriteLine(type.Name);
+            ////Console.WriteLine(type.Assembly.FullName);
+
+            //domain.CreateInstance(type.Assembly.FullName, type.FullName);
+
+            //AppDomain.Unload(domain);
+            //Console.ReadKey();
+
+            //PermissionSet permissions = new PermissionSet(PermissionState.None);
+            //permissions.AddPermission(new FileIOPermission(PermissionState.None));
+            //permissions.AddPermission(new FileIOPermission(FileIOPermissionAccess.NoAccess, "C://"));
+            //var appDomainSetup = new AppDomainSetup();
+            //appDomainSetup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            //AppDomain appDomain = AppDomain.CreateDomain("BabatDomain", null, appDomainSetup, permissions);
+
+            //AppDomain.Unload(appDomain);
+
+
+
+            // console app
+            // butun processler gorunur, sonda ise, New Task, End Task bolmesi olur.
+            // eger New Task sechilse, process adi daxil olunur, ve hemin process ishe dushur.
+            // eger End Task sechilse, process id'si daxil olunur, ve hemin process kill olunur
+
+
+
+
+
+        }
+    }
+
+    class Virus
+    {
+        public Virus() { Console.WriteLine("Salam, men hackerem, geldim ogurluga");  }
+
+        //private void Ogurla() { Console.WriteLine("girdim Disk C'e"); }
+        ~Virus() { Console.WriteLine("Sagol, her sheyi ogurladim gedirem"); }
+    }
+}
---
+++ System Programming/Lesson1Processes/Lesson1Processes/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+//Process.Start("mspaint");
+//Process.Start("notepad");
+//Process.Start("cmd");
+//Process.Start("powershell");
+//Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+
+//var paint = Process.Start("mspaint");
+
+//Thread.Sleep(4000);
+
+//paint.Kill();
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+
+
+//var paints = Process.GetProcessesByName("mspaint");
+
+//foreach (var paint in paints)
+//{
+//    //Console.WriteLine($"{paint.Id} {paint.ProcessName}, {paint.MachineName} {paint.Threads.Count}");
+//    paint.Kill();
+//}
+
+
+//void foo()
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("++++++");
+//        Thread.Sleep(100);
+//    }
+//}
+
+//void boo()
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("------");
+//        Thread.Sleep(100);
+//    }
+//}
+
+
+//var thread = new Thread(() => { foo(); boo(); });
+//thread.Start();
+//foo();
+
+
